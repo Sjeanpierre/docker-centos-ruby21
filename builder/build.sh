ecf06dbe05bed98208ce1c65a4a4f672baa5612b
Stevenson Jean-Pierre <Stevenson.Jean-Pierre@sage.com>
2015-08-14 21:18:15 -0400

    Update build.sh

--- builder/build.sh
+++ builder/build.sh
@@ -1,12 +1,32 @@
 #!/bin/bash -ex
+NOTIFICATION_URL="http://requestb.in/17d6ywk1"
 
 cd /tmp
 mkdir release
 cd release
+#download tarball from github using username and application token, extract to current directory
 curl -Lk --user "$GH_USER:$GH_TOKEN" $TAR_URL | tar -xz
+#set app_git_name to only file in current directory
 APP_GIT_NAME=`echo $(ls)`
+#extracted tarballs are in the form of repoOwner-repoName-shortSha, repo name is just the middle
 REPO_NAME=`echo $APP_GIT_NAME | cut -d'-' --complement -s -f1 | rev | cut -d'-' --complement -s -f1 | rev`
+#create build location and move application
 mkdir -p /var/www/
 cp -r $APP_GIT_NAME /var/www/$REPO_NAME
 cd /var/www/$REPO_NAME
-curl -X POST -d "host=$(echo $HOSTNAME)" -d "fizz=$(ls)" http://requestb.in/17d6ywk1
+#report status to service
+curl -X POST -d "host=$(echo $HOSTNAME)" -d "app=$APP_GIT_NAME" -d "pahse=prebuild" -d "contents=$(ls)" $NOTIFICATION_URL
+
+#build phase
+#remove hard coded ruby version
+sed -i '/^ruby/d' ./Gemfile
+#bundle install with vendoring
+bundle install --deployment --jobs 2 --retry 2
+cp config/database.yml.tmpl config/database.yml
+bundle exec rake assets:precompile
+curl -X POST -d "host=$(echo $HOSTNAME)" -d "app=$APP_GIT_NAME" -d "pahse=prepackage" $NOTIFICATION_URL
+
+#package
+
+
+
