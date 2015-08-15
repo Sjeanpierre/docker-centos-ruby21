9069adfa81b6791a6e7b70be9b8d2671c4b36d0f
Stevenson Jean-Pierre <Stevenson.Jean-Pierre@sage.com>
2015-08-14 20:45:32 -0400

    Update build.sh

--- builder/build.sh
+++ builder/build.sh
@@ -4,6 +4,9 @@ cd /tmp
 mkdir release
 cd release
 curl -Lk --user "$GH_USER:$GH_TOKEN" $TAR_URL | tar -xz
-cd *
-ls
-curl -X POST -d "host=$(echo $HOSTNAME)" -d "fizz=$(ls)" http://requestb.in/1n6jlsc1
+APP_GIT_NAME=`echo $(ls)`
+REPO_NAME=`echo $APP_GIT_NAME | cut -d'-' --complement -s -f1 | rev | cut -d'-' --complement -s -f1 | rev`
+mkdir -p /var/www/
+cp -r $APP_GIT_NAME /var/www/$REPO_NAME
+cd /var/www/$REPO_NAME
+curl -X POST -d "host=$(echo $HOSTNAME)" -d "fizz=$(ls)" http://requestb.in/17d6ywk1
